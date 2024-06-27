17ea3afb0d800f54afed0ae181f40804b8d7ae4e
dev-imran1 <freealltime247@gmail.com>
2024-06-28 04:06:49 +0600

    accessToken && RefreshToken

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import { jwt } from "jsonwebtoken";
 const userSchema = new Schema({
     username: {
         require: true,
@@ -55,5 +56,22 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.method.isPasswordCorrect = async function(password){
-   return bcrypt.compare(password, this.password)
+   return await bcrypt.compare(password, this.password)
 }
+userSchema.method.generateAccessToken = async function(password){
+   const accesToken =  await jwt.sing({_id:this._id,email: this.email}, process.env.ACCESS_TOKEN_SECRECT, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+   });
+   return accesToken
+}
+userSchema.method.generateRefreshToken = async function(password){
+   const refReshToken =  await jwt.sing({_id:this._id,email: this.email}, process.env.REFRESH_TOKEN_SECRECT, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+   });
+   return refReshToken
+}
+
+
+export const User = mongoose.models.User ?? mongoose.model("User", userSchema)
+
+
