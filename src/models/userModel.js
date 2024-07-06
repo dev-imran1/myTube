a740fb60f2422b6b584ba08d1e577a8b11f788b1
dev-imran1 <freealltime247@gmail.com>
2024-07-06 19:44:51 +0600

    pre password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import { jwt } from "jsonwebtoken";
+
 const userSchema = new Schema({
     username: {
         require: true,
@@ -46,32 +47,31 @@ const userSchema = new Schema({
     timestamps: true
 });
 
-
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", function (next) {
     if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }else{
-        return next()
+        this.password =  bcrypt.hash(this.password, 10);
+        next();
+    } else {
+        return next();
     }
 });
 
-userSchema.method.isPasswordCorrect = async function(password){
-   return await bcrypt.compare(password, this.password)
-}
-userSchema.method.generateAccessToken = async function(password){
-   const accesToken =  await jwt.sing({_id:this._id,email: this.email}, process.env.ACCESS_TOKEN_SECRECT, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-   });
-   return accesToken
-}
-userSchema.method.generateRefreshToken = async function(password){
-   const refReshToken =  await jwt.sing({_id:this._id,email: this.email}, process.env.REFRESH_TOKEN_SECRECT, {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-   });
-   return refReshToken
-}
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+};
 
+userSchema.method.generateAccessToken = async function (password) {
+    const accesToken = await jwt.sing({ _id: this._id, email: this.email }, process.env.ACCESS_TOKEN_SECRECT, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    });
+    return accesToken
+};
 
-export const User = mongoose.models.User ?? mongoose.model("User", userSchema)
+userSchema.method.generateRefreshToken = async function (password) {
+    const refReshToken = await jwt.sing({ _id: this._id, email: this.email }, process.env.REFRESH_TOKEN_SECRECT, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    });
+    return refReshToken
+};
 
-
+export const User = mongoose.models.User ?? mongoose.model("User", userSchema);
